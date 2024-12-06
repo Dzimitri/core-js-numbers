e9a5a84e99b25292b33674dea541bde09bb7db1c
Dzmitry <d.prankevich@gmail.com>
2024-12-07 02:17:10 +0300

    fix: deleted comments for example getAngleBetweenVect

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -106,18 +106,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  // скалярное произведение
   const dotProduct = x1 * x2 + y1 * y2;
-
-  // длины векторов
-  const magnitude1 = Math.sqrt(x1 * x1 + y1 * y1);
-  const magnitude2 = Math.sqrt(x2 * x2 + y2 * y2);
-
-  // вычисление угла в радианах
-  const cosTheta = dotProduct / (magnitude1 * magnitude2);
-
-  // arccos для получения угла
-  return Math.acos(cosTheta);
+  const vector1 = Math.sqrt(x1 * x1 + y1 * y1);
+  const vector2 = Math.sqrt(x2 * x2 + y2 * y2);
+  return Math.acos(dotProduct / (vector1 * vector2));
 }
 
 /**
